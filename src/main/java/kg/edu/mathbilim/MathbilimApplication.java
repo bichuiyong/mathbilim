d90f4b2e1ba8decc04de6506af3cfd7e25fce4ec
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-04 20:10:48 +0600

    Initial commit

---
+++ src/main/java/kg/edu/mathbilim/MathbilimApplication.java
@@ -0,0 +1,13 @@
+package kg.edu.mathbilim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathbilimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathbilimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/edu/mathbilim/MathbilimApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.edu.mathbilim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathbilimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
