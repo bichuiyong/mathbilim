92df37598758496652af7658ff4d1bc29ff8a478
chagaiNikita <chagai.nikita@gmail.com>
2025-06-06 01:51:09 +0600

    feat: Implement basic security configuration
    Added `SecurityConfig` for core security settings and `AuthUserDetailsService` for user details retrieval.

--- src/main/java/kg/edu/mathbilim/model/Role.java
+++ src/main/java/kg/edu/mathbilim/model/Role.java
@@ -23,7 +23,7 @@ public class Role {
     @Column(unique = true, nullable = false)
     private String name;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "roles_authorities",
             joinColumns = @JoinColumn(name = "role_id"),
