6c012cdf94b1061f6a75c16809ebdeba94563568
bichuiyong <alexander.tsoy1205@gmail.com>
2025-06-05 19:11:43 +0600

    feat: add all repository based models

---
+++ src/main/java/kg/edu/mathbilim/repository/ContentStatusRepository.java
@@ -0,0 +1,6 @@
+package kg.edu.mathbilim.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentStatusRepository extends JpaRepository<ContentStatusRepository, Long> {
+}
