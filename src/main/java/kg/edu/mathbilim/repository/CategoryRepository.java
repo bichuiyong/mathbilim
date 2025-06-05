a3cf34bb3d6e137233009342b1d2faea3f432722
bichuiyong <alexander.tsoy1205@gmail.com>
2025-06-05 19:11:43 +0600

    feat: add all repository based models

---
+++ src/main/java/kg/edu/mathbilim/repository/CategoryRepository.java
@@ -0,0 +1,6 @@
+package kg.edu.mathbilim.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryRepository, Long> {
+}
---
+++ src/main/java/kg/edu/mathbilim/repository/ContentRepository.java
@@ -0,0 +1,7 @@
+package kg.edu.mathbilim.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentRepository extends JpaRepository<ContentRepository, Long> {
+
+}
