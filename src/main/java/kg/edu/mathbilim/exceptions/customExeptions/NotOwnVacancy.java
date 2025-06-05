52f2b2fd4536a8605a92a6dc9f5807e5672ee1f2
Erik <erikkadyrov32@gmail.com>
2025-06-05 16:51:30 +0600

    hadler

--- src/main/java/kg/edu/mathbilim/exceptions/customExeptions/NotOwnVacancy.java
+++
@@ -1,7 +0,0 @@
-package kg.edu.mathbilim.exceptions.customExeptions;
-
-public class NotOwnVacancy extends RuntimeException {
-    public NotOwnVacancy(String message) {
-        super(message);
-    }
-}
