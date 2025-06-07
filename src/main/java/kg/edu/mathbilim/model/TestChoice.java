87962b6845f18f10aafe6c2b5aeba2c71349ae22
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-07 15:36:58 +0600

    feat: add db models to handle tests

---
+++ src/main/java/kg/edu/mathbilim/model/TestChoice.java
@@ -0,0 +1,38 @@
+package kg.edu.mathbilim.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import kg.edu.mathbilim.model.embedded.TestChoiceId;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "test_choices")
+public class TestChoice {
+    @EmbeddedId
+    private TestChoiceId id;
+
+    @MapsId("testId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.RESTRICT)
+    @JoinColumn(name = "test_id", nullable = false)
+    private Test test;
+
+    @NotNull
+    @Column(name = "time_limit", nullable = false)
+    private Integer timeLimit;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "answered_at")
+    private Instant answeredAt;
+
+}
