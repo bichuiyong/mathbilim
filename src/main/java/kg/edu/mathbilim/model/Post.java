d8dd99c91184fb60207c64b4288b664c7262474a
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-07 15:27:11 +0600

    feat: add db models to handle posts

---
+++ src/main/java/kg/edu/mathbilim/model/Post.java
@@ -0,0 +1,90 @@
+package kg.edu.mathbilim.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.type.SqlTypes;
+
+import java.time.Instant;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @ColumnDefault("nextval('content_id_seq')")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.RESTRICT)
+    @JoinColumn(name = "type_id", nullable = false)
+    private PostType type;
+
+    @Size(max = 500)
+    @NotNull
+    @Column(name = "title", nullable = false, length = 500)
+    private String title;
+
+    @Size(max = 500)
+    @NotNull
+    @Column(name = "slug", nullable = false, length = 500)
+    private String slug;
+
+    @NotNull
+    @Column(name = "content", nullable = false)
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Map<String, Object> content;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @ColumnDefault("0")
+    @Column(name = "view_count")
+    private Long viewCount;
+
+    @ColumnDefault("0")
+    @Column(name = "share_count")
+    private Long shareCount;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.RESTRICT)
+    @JoinColumn(name = "status_id", nullable = false)
+    private ContentStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    @JoinColumn(name = "approved_by")
+    private User approvedBy;
+
+    @ManyToMany(mappedBy = "posts")
+    private Set<Event> events = new LinkedHashSet<>();
+
+    @ManyToMany(mappedBy = "posts")
+    private Set<File> files = new LinkedHashSet<>();
+
+}
