65a33c3780423246f67598e66b3311610501fbce
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-08 15:15:38 +0600

    feat: insert file types and implemented methods to handle file types

--- src/main/java/kg/edu/mathbilim/service/interfaces/reference/types/FileTypeService.java
+++ src/main/java/kg/edu/mathbilim/service/interfaces/reference/types/FileTypeService.java
@@ -1,9 +1,20 @@
 package kg.edu.mathbilim.service.interfaces.reference.types;
 
 import kg.edu.mathbilim.dto.reference.types.FileTypeDto;
+import kg.edu.mathbilim.model.reference.types.FileType;
 
 import java.util.List;
 
 public interface FileTypeService {
     List<FileTypeDto> getAllTypes();
+
+    FileTypeDto findByMimeType(String mimeType);
+
+    FileTypeDto findByExtension(String extension);
+
+    FileType determineFileTypeEntity(String mimeType, String filename);
+
+    FileTypeDto determineFileType(String mimeType, String filename);
+
+    String getFileCategory(String mimeType);
 }
