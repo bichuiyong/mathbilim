a8b9897efd349f529f5f0d245f307a441f245183
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-10 03:45:26 +0600

    feat: configured Security Config for post uploading

--- src/main/java/kg/edu/mathbilim/config/SecurityConfig.java
+++ src/main/java/kg/edu/mathbilim/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                         .permitAll())
 
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/posts/create/**").authenticated()
                         .anyRequest().permitAll());
 
         return http.build();
