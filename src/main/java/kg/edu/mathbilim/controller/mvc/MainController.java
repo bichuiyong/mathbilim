2bb8e0668d230544da49a46c221ac8f6e5392420
Aisha Orozbekova <aisha.orozbekova@alatoo.edu.kg>
2025-06-09 12:10:05 +0600

    feat: separate controllers
    * feat: created base mvc controllers
    
    * chore: moved test page to separate directory
    
    * chore: moved olymp page to separate directory
    
    * chore: moved news and publication page to separate directory
    
    * chore: moved book page to separate directory

--- src/main/java/kg/edu/mathbilim/controller/mvc/MainController.java
+++ src/main/java/kg/edu/mathbilim/controller/mvc/MainController.java
@@ -7,44 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class MainController {
-
     @GetMapping
     public String index() {
         return "main";
     }
-
-    @GetMapping("books")
-    public String books() {
-        return "books";
-    }
-
-    @GetMapping("olymp")
-    public String olymp() {
-        return "olymp";
-    }
-
-    @GetMapping("olymp-details")
-    public String olympDetails() {
-        return "olymp-details";
-    }
-
-    @GetMapping("events")
-    public String eventList() {
-        return "events/event-list";
-    }
-
-    @GetMapping("event/details")
-    public String eventDetail() {
-        return "events/event-details";
-    }
-
-    @GetMapping("test-details")
-    public String testDetails() { return "test-details"; }
-
-    @GetMapping("tests")
-    public String tests() { return "tests"; }
-
-    @GetMapping("test-ready")
-    public String testReady() { return "test-ready"; }
-
 }
