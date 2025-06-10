1e8d4e0c259207e6095c97ed97642a725d971a0f
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-10 19:09:18 +0600

    feat: add page to create organization

--- src/main/java/kg/edu/mathbilim/config/SecurityConfig.java
+++ src/main/java/kg/edu/mathbilim/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                         .permitAll())
 
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/posts/create/**").authenticated()
+                        .requestMatchers("/posts/create/**", "/organizations/create/**").authenticated()
                         .anyRequest().permitAll());
 
         return http.build();
