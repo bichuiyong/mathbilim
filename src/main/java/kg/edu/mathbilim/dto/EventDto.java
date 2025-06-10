e15c2540cdf3104934dc33ca4f7164fa946c5f0e
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-11 01:09:54 +0600

    feat: add custom data range validator

--- src/main/java/kg/edu/mathbilim/dto/EventDto.java
+++ src/main/java/kg/edu/mathbilim/dto/EventDto.java
@@ -1,10 +1,15 @@
 package kg.edu.mathbilim.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import kg.edu.mathbilim.enums.ContentStatus;
+import kg.edu.mathbilim.validation.annotation.ValidDateTimeRange;
 import lombok.*;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,11 +18,17 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@ValidDateTimeRange(
+        startDateTimeField = "startDate",
+        endDateTimeField = "endDate"
+)
 public class EventDto {
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String content;
 
     private FileDto mainImage;
@@ -26,19 +37,28 @@ public class EventDto {
 
     private ContentStatus status;
 
+    @NotNull
+    @Future
     private LocalDateTime startDate;
 
+    @Future
     private LocalDateTime endDate;
 
+    @NotNull
     private EventTypeDto type;
 
+    @NotNull
     private UserDto user;
 
-    private UserDto approvedBy;
+    @Builder.Default
+    private UserDto approvedBy = null;
 
-    private Instant createdAt;
+    @Builder.Default
+    private Instant createdAt = Instant.now();
 
-    private Instant updatedAt;
+    @Builder.Default
+    private Instant updatedAt = Instant.now();
 
-    private Set<FileDto> files;
+    @Builder.Default
+    private Set<FileDto> files = new LinkedHashSet<>();
 }
