eacec8b7bd329c53fce4e975e52026ad38bb90cf
Erik <erikkadyrov32@gmail.com>
2025-06-10 20:50:44 +0600

    ne nujen okaz

--- src/main/java/kg/edu/mathbilim/MathbilimApplication.java
+++ src/main/java/kg/edu/mathbilim/MathbilimApplication.java
@@ -2,17 +2,9 @@ package kg.edu.mathbilim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MathbilimApplication {
-
-    static {
-        Dotenv dotenv = Dotenv.configure().load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(MathbilimApplication.class, args);
