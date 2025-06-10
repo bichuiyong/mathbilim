b8453876ac147e93f8d8700ac2627184a79d4cb3
Erik <erikkadyrov32@gmail.com>
2025-06-10 13:56:46 +0600

    ability to add database via env file

--- src/main/java/kg/edu/mathbilim/MathbilimApplication.java
+++ src/main/java/kg/edu/mathbilim/MathbilimApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MathbilimApplication {
 
     public static void main(String[] args) {
+
+        io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.configure().load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(MathbilimApplication.class, args);
     }
 
