59a913e250ceabcb03a7dc22acf5920fb43709f6
chagaiNikita <chagai.nikita@gmail.com>
2025-06-12 17:53:57 +0600

    feat: implement localeConfig and user type changing
    * feat: Set up LocaleConfig to change and save language in Cookie
    
    * feat: added the ability for the user to change the account type

--- src/main/java/kg/edu/mathbilim/dto/UserEditDto.java
+++ src/main/java/kg/edu/mathbilim/dto/UserEditDto.java
@@ -1,14 +1,22 @@
 package kg.edu.mathbilim.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEditDto {
     @NotBlank(message = "Имя не может быть пустым")
     private String name;
 
     private String surname;
+
+    @NotNull(message = "Выберите тип аккаунта")
+    @Positive(message = "Выберите тип аккаунта")
+    private Integer typeId;
 }
