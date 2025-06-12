7db49d3e0410fd7ec31c3d18e6c445eb4e285e3a
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-12 19:46:36 +0600

    feat: implement event translation service

---
+++ src/main/java/kg/edu/mathbilim/service/interfaces/event/EventTranslationService.java
@@ -0,0 +1,37 @@
+package kg.edu.mathbilim.service.interfaces.event;
+
+import kg.edu.mathbilim.dto.event.EventTranslationDto;
+import kg.edu.mathbilim.model.event.EventTranslation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface EventTranslationService {
+    List<EventTranslationDto> getTranslationsByEventId(Long eventId);
+
+    EventTranslation getTranslationEntity(Long eventId, String languageCode);
+
+    EventTranslationDto getTranslation(Long eventId, String languageCode);
+
+    List<EventTranslationDto> getTranslationsByLanguage(String languageCode);
+
+    @Transactional
+    EventTranslationDto createTranslation(EventTranslationDto dto);
+
+    @Transactional
+    EventTranslationDto updateTranslation(Long eventId, String languageCode, String title, String content);
+
+    @Transactional
+    EventTranslationDto upsertTranslation(EventTranslationDto dto);
+
+    @Transactional
+    void deleteTranslation(Long eventId, String languageCode);
+
+    @Transactional
+    void deleteAllTranslationsByEventId(Long eventId);
+
+    boolean existsTranslation(Long eventId, String languageCode);
+
+    @Transactional
+    void saveTranslations(Long eventId, List<EventTranslationDto> translations);
+}
