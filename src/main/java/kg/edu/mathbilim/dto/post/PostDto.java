2372a278f59d03bd0076ae5eb5ff251060e8fe0c
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-13 16:57:51 +0600

    refactor: change type field to long

--- src/main/java/kg/edu/mathbilim/dto/post/PostDto.java
+++ src/main/java/kg/edu/mathbilim/dto/post/PostDto.java
@@ -3,14 +3,16 @@ package kg.edu.mathbilim.dto.post;
 import jakarta.validation.constraints.NotNull;
 import kg.edu.mathbilim.dto.FileDto;
 import kg.edu.mathbilim.dto.user.UserDto;
-import kg.edu.mathbilim.dto.post.post_type.PostTypeDto;
 import kg.edu.mathbilim.enums.ContentStatus;
+import kg.edu.mathbilim.enums.Language;
 import kg.edu.mathbilim.validation.annotation.AtLeastOneTranslationRequired;
 import lombok.*;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -21,7 +23,7 @@ public class PostDto {
     private Long id;
 
     @NotNull
-    private PostTypeDto type;
+    private Long typeId;
 
     private UserDto user;
 
@@ -48,6 +50,16 @@ public class PostDto {
     List<FileDto> files = new ArrayList<>();
 
     @AtLeastOneTranslationRequired
-    private List<PostTranslationDto> postTranslations;
+    @Builder.Default
+    private List<PostTranslationDto> postTranslations = createDefaultTranslations();
 
+    private static List<PostTranslationDto> createDefaultTranslations() {
+        return Arrays.stream(Language.values())
+                .map(lang -> PostTranslationDto.builder()
+                        .languageCode(lang.getCode())
+                        .title("")
+                        .content("")
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
