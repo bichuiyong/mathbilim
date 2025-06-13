1612e6ba4c5d89357dcbc8c1dec8c29fc3adb674
bichuiyong <alexander.tsoy1205@gmail.com>
2025-06-13 17:41:34 +0600

    Added migration for events

---
+++ src/main/resources/db/changelog/initial/1106/037_change_admin_data.sql
@@ -0,0 +1,11 @@
+UPDATE users
+SET is_email_verified = true
+WHERE email = 'user@gmail.com';
+
+UPDATE users
+SET is_email_verified = true
+WHERE email = 'moder@gmail.com';
+
+UPDATE users
+SET is_email_verified = true
+WHERE email = 'admin@gmail.com';
