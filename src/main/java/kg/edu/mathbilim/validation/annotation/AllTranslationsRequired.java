0350d21e162e57d0970166f7e904eba0bda8cf98
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-13 16:00:04 +0600

    feat: created custom annotation to create type's with all translates

--- src/main/java/kg/edu/mathbilim/validation/annotation/AllTranslationsRequired.java
+++ src/main/java/kg/edu/mathbilim/validation/annotation/AllTranslationsRequired.java
@@ -2,18 +2,18 @@ package kg.edu.mathbilim.validation.annotation;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import kg.edu.mathbilim.validation.validator.AtLeastOneTranslationValidator;
+import kg.edu.mathbilim.validation.validator.AllTranslationsRequiredValidator;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.FIELD})
+@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = AtLeastOneTranslationValidator.class)
-public @interface AtLeastOneTranslation {
-    String message() default "Должен быть заполнен хотя бы один перевод";
+@Constraint(validatedBy = AllTranslationsRequiredValidator.class)
+public @interface AllTranslationsRequired {
+    String message() default "Все переводы должны быть заполнены";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
