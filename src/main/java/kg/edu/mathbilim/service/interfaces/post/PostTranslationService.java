b662df9b4d2cb48cfa14dfaa64b5ff67d29fd4fd
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-06-13 16:56:59 +0600

    feat: implement post translation services

--- src/main/java/kg/edu/mathbilim/service/interfaces/post/PostTranslationService.java
+++ src/main/java/kg/edu/mathbilim/service/interfaces/post/PostTranslationService.java
@@ -1,4 +1,38 @@
 package kg.edu.mathbilim.service.interfaces.post;
 
+import kg.edu.mathbilim.dto.post.PostTranslationDto;
+import kg.edu.mathbilim.model.post.PostTranslation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Set;
+
 public interface PostTranslationService {
+    List<PostTranslationDto> getTranslationsByPostId(Long postId);
+
+    PostTranslation getTranslationEntity(Long postId, String languageCode);
+
+    PostTranslationDto getTranslation(Long postId, String languageCode);
+
+    List<PostTranslationDto> getTranslationsByLanguage(String languageCode);
+
+    @Transactional
+    PostTranslationDto createTranslation(PostTranslationDto dto);
+
+    @Transactional
+    PostTranslationDto updateTranslation(Long postId, String languageCode, String title, String content);
+
+    @Transactional
+    PostTranslationDto upsertTranslation(PostTranslationDto dto);
+
+    @Transactional
+    void deleteTranslation(Long postId, String languageCode);
+
+    @Transactional
+    void deleteAllTranslationsByPostId(Long postId);
+
+    boolean existsTranslation(Long postId, String languageCode);
+
+    @Transactional
+    void saveTranslations(Long postId, Set<PostTranslationDto> translations);
 }
