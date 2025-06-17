351f70358f7118b405dd7b9cd2335c86846e08d7
chagaiNikita <chagai.nikita@gmail.com>
2025-06-18 01:14:44 +0600

    feat: wrote migrations to create the blogs table blog_translations created the comments and blog_comments table

---
+++ src/main/resources/db/changelog/initial/1706/043_create_blogs_table.sql
@@ -0,0 +1,24 @@
+-- changelog Nikita: 043 create blogs table
+
+create table blogs(
+    id bigint primary key ,
+    creator_id bigint not null,
+    constraint fk_blog_creator_id foreign key (creator_id)
+        references users(id)
+        on delete restrict
+        on update cascade,
+    approved_id bigint,
+    constraint fk_blog_approved_id foreign key (approved_id)
+        references users(id)
+        on delete restrict
+        on update cascade,
+    status_id integer not null,
+    constraint fk_blog_status_id foreign key (status_id)
+        references content_statuses(id)
+        on delete restrict
+        on update cascade,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+    view_count bigint not null default 0,
+    share_count bigint not null default 0
+);
---
+++ src/main/resources/db/changelog/initial/1706/045_create_comments_table.sql
@@ -0,0 +1,13 @@
+-- changelog Nikita 045 create comments Table
+
+create table comments(
+    id bigint primary key,
+    author_id bigint not null ,
+    constraint fk_comments_author_id foreign key (author_id)
+        references users(id)
+        on delete restrict
+        on update cascade,
+    content varchar(255) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+);
