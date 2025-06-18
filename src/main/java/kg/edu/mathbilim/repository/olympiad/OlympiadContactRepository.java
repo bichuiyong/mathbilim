4070be42c83fa0dee7c04921760246d187251a59
Adis <priv4484@gmail.com>
2025-06-18 14:06:31 +0600

    feat: add JPA repositories for olympiad domain
    Added repository interfaces for:
    - Olympiad
    - StageOlympiad
    - ResultStage
    - ContactOlympiad

---
+++ src/main/java/kg/edu/mathbilim/repository/olympiad/OlympiadContactRepository.java
@@ -0,0 +1,7 @@
+package kg.edu.mathbilim.repository.olympiad;
+
+import kg.edu.mathbilim.model.olympiad.OlympiadContact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OlympiadContactRepository extends JpaRepository<OlympiadContact, Integer> {
+}
---
+++ src/main/java/kg/edu/mathbilim/repository/olympiad/OlympiadStageRepository.java
@@ -0,0 +1,7 @@
+package kg.edu.mathbilim.repository.olympiad;
+
+import kg.edu.mathbilim.model.olympiad.OlympiadStage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OlympiadStageRepository extends JpaRepository<OlympiadStage, Integer> {
+}
