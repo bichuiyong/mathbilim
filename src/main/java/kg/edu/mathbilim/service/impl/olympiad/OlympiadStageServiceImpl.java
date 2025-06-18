d1cad49f841558543ff0937cb9b09ab93a1fbccc
Adis <priv4484@gmail.com>
2025-06-18 14:08:50 +0600

    feat: add service layer and create methods for olympiad domain
    Implemented service classes and creation logic for:
    - Olympiad
    - StageOlympiad
    - ResultStage
    - ContactOlympiad

---
+++ src/main/java/kg/edu/mathbilim/service/impl/olympiad/OlympiadStageServiceImpl.java
@@ -0,0 +1,43 @@
+package kg.edu.mathbilim.service.impl.olympiad;
+
+import kg.edu.mathbilim.dto.olympiad.OlympiadCreateDto;
+import kg.edu.mathbilim.model.olympiad.Olympiad;
+import kg.edu.mathbilim.model.olympiad.OlympiadStage;
+import kg.edu.mathbilim.repository.olympiad.OlympiadStageRepository;
+import kg.edu.mathbilim.service.interfaces.olympiad.OlympiadStageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+@RequiredArgsConstructor
+public class OlympiadStageServiceImpl implements OlympiadStageService {
+    private final OlympiadStageRepository repository;
+
+
+    @Override
+    public void save(OlympiadCreateDto dto, Olympiad olympiad) {
+        if (dto.getStages() == null) return;
+
+        AtomicInteger order = new AtomicInteger(1);
+        dto.getStages().forEach(s -> {
+            OlympiadStage stage = OlympiadStage.builder()
+                    .name(s.getName())
+                    .description(s.getDescription())
+                    .eventStartDate(s.getEventStartDate())
+                    .eventEndDate(s.getEventEndDate())
+                    .registrationStart(s.getRegistrationStart())
+                    .registrationEnd(s.getRegistrationEnd())
+                    .olympiad(olympiad)
+                    .location(s.getLocation())
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .stageOrder(order.getAndIncrement())
+                    .build();
+            repository.saveAndFlush(stage);
+        });
+
+    }
+}
