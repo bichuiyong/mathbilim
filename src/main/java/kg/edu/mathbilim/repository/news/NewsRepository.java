bc289466d5c267c3e1df881d34a25b310ad42f11
Adis <priv4484@gmail.com>
2025-06-25 16:49:51 +0600

    feat: add repository methods for fetching news by query and language

--- src/main/java/kg/edu/mathbilim/repository/news/NewsRepository.java
+++ src/main/java/kg/edu/mathbilim/repository/news/NewsRepository.java
@@ -2,6 +2,7 @@ package kg.edu.mathbilim.repository.news;
 
 import kg.edu.mathbilim.model.news.News;
 import kg.edu.mathbilim.repository.abstracts.BaseContentRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -19,4 +20,22 @@ public interface NewsRepository extends JpaRepository<News, Long>, BaseContentRe
     @Modifying
     @Query("UPDATE News b SET b.shareCount = b.shareCount + 1 WHERE b.id = :blogId")
     void incrementShareCount(@Param("blogId") Long blogId);
+
+
+    @Query(value = "SELECT DISTINCT n FROM News n " +
+            "JOIN n.newsTranslations t " +
+            "where t.id.languageCode = :lang")
+    Page<News> findByNewsWithLang(int page, int size, String sortBy, String sortDirection, String lang);
+
+    @Query(value = """
+            SELECT DISTINCT n FROM News n
+                        JOIN n.newsTranslations t
+                        where t.id.languageCode = :lang
+                        and LOWER(t.title) LIKE LOWER(CONCAT('%', :query, '%'))
+            """)
+    Page<News> findNewsByQuery(String query, int page, int size, String sortBy, String sortDirection, String lang);
+
+
+    Page<News> findAllNews(int page, int size, String sortBy, String sortDirection);
+
 }
