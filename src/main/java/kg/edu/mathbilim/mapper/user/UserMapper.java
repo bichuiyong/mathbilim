2bfa3a9c52d181066006b422c875810c59c936b7
chagaiNikita <chagai.nikita@gmail.com>
2025-08-10 02:54:23 +0600

    fix: Fixed user data leak, fixed horizontal scroll, added a button to create organizations in the profile

--- src/main/java/kg/edu/mathbilim/mapper/user/UserMapper.java
+++ src/main/java/kg/edu/mathbilim/mapper/user/UserMapper.java
@@ -10,6 +10,14 @@ import org.mapstruct.Mapping;
 public interface UserMapper {
     User toEntity(UserDto dto);
 
+    @Mapping(target = "email", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "isEmailVerified", ignore = true)
+    @Mapping(target = "enabled", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "type", ignore = true)
+    @Mapping(target = "role", ignore = true)
     UserDto toDto(User user);
 
     @Mapping(target = "typeId", source = "type.id")
