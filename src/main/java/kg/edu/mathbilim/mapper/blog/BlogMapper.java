b2eb1d6d20634353beff854acb2b0c263c27581d
Adis <priv4484@gmail.com>
2025-08-14 01:32:46 +0600

    feat: Update mappers for news, blog posts, events, and posts - Add target mappings to ensure proper data transformation - Support consistent mapping for all content types - Prepare data for seamless use in services and views

--- src/main/java/kg/edu/mathbilim/mapper/blog/BlogMapper.java
+++ src/main/java/kg/edu/mathbilim/mapper/blog/BlogMapper.java
@@ -2,13 +2,20 @@ package kg.edu.mathbilim.mapper.blog;
 
 import kg.edu.mathbilim.dto.blog.BlogDto;
 import kg.edu.mathbilim.mapper.BaseMapper;
+import kg.edu.mathbilim.mapper.news.NewsTranslationMapper;
 import kg.edu.mathbilim.model.blog.Blog;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        builder = @Builder(disableBuilder = true),
+        uses = { BlogTranslationMapper.class })
 public interface BlogMapper extends BaseMapper<Blog, BlogDto> {
 
+    @Mapping(target = "blogTranslations", source = "blogTranslations")
     Blog toEntity(BlogDto blogDto);
 
+    @Mapping(target = "blogTranslations", source = "blogTranslations")
     BlogDto toDto(Blog blog);
 }
