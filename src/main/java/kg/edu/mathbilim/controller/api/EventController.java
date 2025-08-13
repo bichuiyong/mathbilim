00c5ba530776636095b43864c7bc0127e021e550
Adis <priv4484@gmail.com>
2025-08-14 01:34:07 +0600

    feat: Add API endpoints for news, blog posts, publications, events, and olympiads - Implement endpoints to fetch all items from the database - Support retrieval of single items for detailed view - Enable asynchronous responses for smooth frontend integration - Ensure endpoints work consistently across all content types

--- src/main/java/kg/edu/mathbilim/controller/api/EventController.java
+++ src/main/java/kg/edu/mathbilim/controller/api/EventController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 @Slf4j
 @RestController("restEvent")
 @RequestMapping("api/event")
@@ -30,7 +29,6 @@ public class EventController {
         log.info(allEvent.getTotalElements() + " events found");
         return ResponseEntity.ok(allEvent);
 }
-
 
     @PostMapping("{id}/share")
     public ResponseEntity<Void> shareEvent(@PathVariable Long id) {
