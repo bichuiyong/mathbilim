febf8508c500eed6813f256af0091c80a4599c2a
Adis <priv4484@gmail.com>
2025-08-24 20:03:07 +0600

    fix(mapper-content): add new mapping for content

--- src/main/java/kg/edu/mathbilim/mapper/blog/BlogMapper.java
+++ src/main/java/kg/edu/mathbilim/mapper/blog/BlogMapper.java
@@ -4,11 +4,13 @@ import kg.edu.mathbilim.dto.blog.BlogDto;
 import kg.edu.mathbilim.mapper.BaseMapper;
 import kg.edu.mathbilim.model.blog.Blog;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BlogMapper extends BaseMapper<Blog, BlogDto> {
 
     Blog toEntity(BlogDto blogDto);
 
+    @Mapping(target = "deleted", source = "deleted")
     BlogDto toDto(Blog blog);
 }
