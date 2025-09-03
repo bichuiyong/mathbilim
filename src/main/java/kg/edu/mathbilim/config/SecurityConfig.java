0dc1b2bd82f7bd7b39d61c4f0871ddfaa24fd901
Erik <erikkadyrov32@gmail.com>
2025-09-04 00:57:31 +0600

    zone

--- src/main/java/kg/edu/mathbilim/config/SecurityConfig.java
+++ src/main/java/kg/edu/mathbilim/config/SecurityConfig.java
@@ -8,8 +8,10 @@ import kg.edu.mathbilim.service.impl.auth.UserEmailHandler;
 import kg.edu.mathbilim.service.impl.auth.UserTypeHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,9 +37,17 @@ public class SecurityConfig {
     private String rememberMeKey;
 
     @Bean
+    public FilterRegistrationBean<TimeZoneFilter> timeZoneFilterRegistration(TimeZoneFilter timeZoneFilter) {
+        FilterRegistrationBean<TimeZoneFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(timeZoneFilter);
+        registration.addUrlPatterns("/*");
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registration;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .addFilterBefore(timeZoneFilter,UsernamePasswordAuthenticationFilter.class)
                 .addFilterAfter(userEmailHandler, UsernamePasswordAuthenticationFilter.class)
                 .addFilterAfter(userTypeHandler, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(ex -> ex
